4b4e0b863d28d0aa2779c614239debe8e66e9f80
PrasantiKadiyala <prasanti.kadiyala@tcs.com>
2020-09-26 00:01:02 +0530

    FirstCommit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Workspace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Successfully installed .NET");
+        }
+    }
+}
